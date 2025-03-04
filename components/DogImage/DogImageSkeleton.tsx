87d7af43114adb6249124e19388a29bb9cce48e9
Puc3k <kacperplocienniczak20@gmail.com>
2025-03-05 00:24:59 +0100

    feat: add doge image skeleton

---
+++ components/DogImage/DogImageSkeleton.tsx
@@ -0,0 +1,7 @@
+export default function DogImageSkeleton () {
+  return (
+    <div
+      className="w-[300px] h-[300px] md:w-[400px] md:h-[400px] rounded-lg bg-gray-300 animate-pulse"
+    />
+  )
+}
