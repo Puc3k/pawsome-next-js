692813056d5bd08d88a3b3b26223c9d5fafc02f8
Puc3k <kacperplocienniczak20@gmail.com>
2025-03-14 00:25:58 +0100

    refactor: remove duplicated content

--- components/Quiz/QuizContent.tsx
+++ components/Quiz/QuizContent.tsx
@@ -32,110 +32,89 @@ const QuizContent: React.FC<QuizContentProps> = ({
   const leftImage = quizState.pool[0]
   const rightImage = quizState.pool[1]
 
-  if (isLoading || !leftImage || !rightImage && quizState.pool.length > 1) {
-    return (
-      <section
-        className="relative mx-auto pt-8 pb-16 overflow-hidden bg-gray-50">
-        <div>
-          <h1
-            className="text-5xl text-center font-bold mb-4 mt-6 font-[Poppins] tracking-wide">
-            Which dog image wins?
-          </h1>
-          <p className="text-center text-lg font-sans mb-6 tracking-wide">
-            Choose your favorite!
-          </p>
-        </div>
-        <p className="text-center mb-6">Loading images...</p>
-        <div className="flex justify-center items-center gap-12">
-          <DogImageSkeleton/>
-          <div className="hidden md:block relative z-20">
-            <span
-              className="text-3xl font-bold text-gray-700 font-mono">VS</span>
-          </div>
-          <DogImageSkeleton/>
-        </div>
-      </section>
-    )
-  }
-
-  if (error) {
-    return <section
-      className="relative mx-auto pt-8 pb-16 overflow-hidden bg-gray-50">
-      <div>
-        <h1
-          className="text-5xl text-center font-bold mb-4 mt-6 font-[Poppins] tracking-wide">
-          Which dog image wins?
-        </h1>
-        <p className="text-center text-lg font-sans mb-6 tracking-wide">
-          Choose your favorite!
-        </p>
-      </div>
-      <p className="text-center text-red-500">Error loading images.</p>
-    </section>
-  }
-
-  if (quizState.pool.length === 1) {
-    return <section
-      className="relative mx-auto pt-8 pb-16 overflow-hidden bg-gray-50">
-      <div
-        className={ `inset-0 opacity-3 pointer-events-none ${ classes.pawBackground }` }></div>
-      <div className="relative z-10">
-        <WinnerDogImage imageUrl={ quizState.pool[0] } onReset={ handleReset }/>
-      </div>
-    </section>
-  }
-
+  const renderQuizHeader = () => (
+    <div>
+      <h1
+        className="text-5xl text-center font-bold mb-4 mt-6 font-[Poppins] tracking-wide">
+        Which dog image wins?
+      </h1>
+      <p className="text-center text-lg font-sans mb-6 tracking-wide">
+        Choose your favorite!
+      </p>
+    </div>
+  )
   return (
     <section className="relative mx-auto pt-8 pb-16 overflow-hidden bg-gray-50">
       <div
         className={ `inset-0 opacity-3 pointer-events-none ${ classes.pawBackground }` }></div>
       <div className="relative z-10">
-        <>
-          <div>
-            <h1
-              className="text-5xl text-center font-bold mb-4 mt-6 font-[Poppins] tracking-wide">
-              Which dog image wins?
-            </h1>
-            <p className="text-center text-lg font-sans mb-6 tracking-wide">
-              Choose your favorite!
-            </p>
-          </div>
-          <ProgressBar current={ currentRound } total={ totalRounds }/>
-          <div
-            className="relative flex flex-col md:flex-row items-center justify-center gap-6 md:gap-12">
-            <DogImage
-              key={ leftImage }
-              imageUrl={ leftImage }
-              isSelected={ selectedImage === leftImage }
-              onClick={ () => setSelectedImage(leftImage) }
-              onAnimationComplete={ () => handleImageSelected(leftImage,
-                rightImage) }
-              alt="First dog image"
-            />
-            <div className="hidden md:block relative z-20">
-              <div
-                className="flex items-center justify-center w-20 h-20 bg-white bg-opacity-80 rounded-full shadow-md border-2 border-gray-300">
+        { isLoading && (
+          <>
+            { renderQuizHeader() }
+            <p className="text-center mb-6">Loading images...</p>
+            <div className="flex justify-center items-center gap-12">
+              <DogImageSkeleton/>
+              <div className="hidden md:block relative z-20">
                 <span
                   className="text-3xl font-bold text-gray-700 font-mono">VS</span>
               </div>
+              <DogImageSkeleton/>
             </div>
-            <DogImage
-              key={ rightImage }
-              imageUrl={ rightImage }
-              isSelected={ selectedImage === rightImage }
-              onClick={ () => setSelectedImage(rightImage) }
-              onAnimationComplete={ () => handleImageSelected(rightImage,
-                leftImage) }
-              alt="Second dog image"
-            />
-          </div>
-          <button
-            onClick={ handleReset }
-            className="mt-6 mx-auto block px-6 py-2 bg-yellow-400 hover:bg-yellow-500 text-white rounded-full text-lg font-semibold shadow-md transition"
-          >
-            Reset
-          </button>
-        </>
+          </>
+        ) }
+
+        { error && (
+          <>
+            { renderQuizHeader() }
+            <p className="text-center text-red-500">Error loading images.</p>
+          </>
+        ) }
+
+        { quizState.pool.length === 1 && (
+          <WinnerDogImage imageUrl={ quizState.pool[0] }
+                          onReset={ handleReset }/>
+        ) }
+
+        { quizState.pool.length > 1 && !isLoading && !error && (
+          <>
+            { renderQuizHeader() }
+            <ProgressBar current={ currentRound } total={ totalRounds }/>
+            <div
+              className="relative flex flex-col md:flex-row items-center justify-center gap-6 md:gap-12">
+              <DogImage
+                key={ leftImage }
+                imageUrl={ leftImage }
+                isSelected={ selectedImage === leftImage }
+                onClick={ () => setSelectedImage(leftImage) }
+                onAnimationComplete={ () => handleImageSelected(leftImage,
+                  rightImage) }
+                alt="First dog image"
+              />
+              <div className="hidden md:block relative z-20">
+                <div
+                  className="flex items-center justify-center w-20 h-20 bg-white bg-opacity-80 rounded-full shadow-md border-2 border-gray-300">
+                  <span
+                    className="text-3xl font-bold text-gray-700 font-mono">VS</span>
+                </div>
+              </div>
+              <DogImage
+                key={ rightImage }
+                imageUrl={ rightImage }
+                isSelected={ selectedImage === rightImage }
+                onClick={ () => setSelectedImage(rightImage) }
+                onAnimationComplete={ () => handleImageSelected(rightImage,
+                  leftImage) }
+                alt="Second dog image"
+              />
+            </div>
+            <button
+              onClick={ handleReset }
+              className="mt-6 mx-auto block px-6 py-2 bg-yellow-400 hover:bg-yellow-500 text-white rounded-full text-lg font-semibold shadow-md transition"
+            >
+              Reset
+            </button>
+          </>
+        ) }
       </div>
     </section>
   )
