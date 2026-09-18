09c58bb551e6c492dc09d000210ce44b083e54e2
Pucek <kacperplocienniczak20@gmail.com>
2026-09-19 00:07:26 +0200

    fix: display placeholders when image returns 404, improve img optimization

--- components/TopDog/TopDogItem.tsx
+++ components/TopDog/TopDogItem.tsx
@@ -3,12 +3,17 @@ import { useState } from 'react'
 import Image from 'next/image'
 import TopDogImageSkeleton from '@/components/TopDog/TopDogImageSkeleton'
 import { TopDogItemProps } from '@/types/tournament'
+import logoImg from '@/assets/logo.webp'
 
 const TopDogItem = ({ image, index }: TopDogItemProps) => {
   const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState<boolean>(false)
 
   const handleLoad = () => setIsLoading(false)
-  const handleError = () => setIsLoading(false)
+  const handleError = () => {
+    setIsLoading(false)
+    setHasError(true)
+  }
 
   const itemData: {
     [key: number]: {
@@ -17,6 +22,7 @@ const TopDogItem = ({ image, index }: TopDogItemProps) => {
       altText: string,
       titleText: string,
       order: string,
+      sizes: string,
     }
   } = {
     0: {
@@ -24,38 +30,46 @@ const TopDogItem = ({ image, index }: TopDogItemProps) => {
       textStyle: 'text-xl font-bold text-yellow-600',
       altText: 'Top 1 Dog',
       titleText: '🏆 #1 Top Dog',
-      order: 'order-1 md:order-[inherit]'
+      order: 'order-1 md:order-[inherit]',
+      sizes: '(max-width: 768px) 100vw, 240px'
     },
     1: {
       imageStyle: 'w-full md:w-60 md:h-60 border-slate-400/50 shadow-lg',
       textStyle: 'font-semibold',
       altText: 'Top 2 Dog',
       titleText: '#2 Runner Up',
-      order: 'order-2 md:order-[inherit]'
+      order: 'order-2 md:order-[inherit]',
+      sizes: '(max-width: 768px) 100vw, 240px'
     },
     2: {
       imageStyle: 'w-full md:w-60 md:h-60 order:3 md:order-[inherit] border-amber-600/50 shadow-lg',
       textStyle: 'font-semibold',
       altText: 'Top 3 Dog',
       titleText: '#3 Fan Favorite',
-      order: 'order-3 md:order-[inherit]'
+      order: 'order-3 md:order-[inherit]',
+      sizes: '(max-width: 768px) 100vw, 240px'
     },
   }
 
-  const { imageStyle, textStyle, altText, titleText, order } = itemData[index]
+  const { imageStyle, textStyle, altText, titleText, order, sizes } = itemData[index]
 
+  const imgSrc = hasError ? logoImg : image?._id || logoImg
   return (
     <div className={`flex flex-col items-center relative w-full md:w-[inherit] px-6 md:p-0 ${ order }`}>
         { isLoading && <TopDogImageSkeleton/> }
         <Image
-          src={ image?._id || '' }
+          priority
+          src={ imgSrc}
           alt={ altText }
           width="600"
           height="600"
-          priority
+          sizes={ sizes }
           onLoad={ handleLoad }
           onError={ handleError }
-          style={ { display: isLoading ? 'none' : 'block' } }
+          style={ {
+            display: isLoading ? 'none' : 'block',
+            opacity: hasError ? 0.3 : 1
+          } }
           className={`object-cover rounded-3xl overflow-hidden shadow-lg border-4 ${ imageStyle }`}
         />
       <p className={ `mt-4 text-lg font-mono ${ textStyle }` }>
